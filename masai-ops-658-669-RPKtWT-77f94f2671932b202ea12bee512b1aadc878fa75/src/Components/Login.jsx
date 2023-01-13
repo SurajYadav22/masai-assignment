20010dee10f18ad4dc0568bca2f05727b26ea4c0
Suraj <surajybtg@gmail.com>
2023-01-13 19:41:09 +0530

    Done all things

--- masai-ops-658-669-RPKtWT-77f94f2671932b202ea12bee512b1aadc878fa75/src/Components/Login.jsx
+++ masai-ops-658-669-RPKtWT-77f94f2671932b202ea12bee512b1aadc878fa75/src/Components/Login.jsx
@@ -1,57 +1,65 @@
-import { React, useState } from "react";
-// import { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+// import "./login.css";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
-  const initialvalues = { name: "", password: "" };
-  const [inputValues, setInputValues] = useState(initialvalues);
+  const navigate = useNavigate();
   const [error, setError] = useState("");
 
+  const initialvalues = { name: "", password: "" };
+  const [inputValues, setInputValues] = useState(initialvalues);
   const handleChange = (inp) => {
     const { name, value } = inp.target;
     setInputValues({ ...inputValues, [name]: value });
     console.log(inputValues);
   };
-  const handleSubmit = (e) => {
+  const handlelogin = (e) => {
     e.preventDefault();
     fetch("http://localhost:8080/users")
       .then((res) => res.json())
       .then((res) => {
-        let test2 = res.filter((el) => {
+        let result = res.filter((el) => {
           return (
             el.name === inputValues.name && el.password === inputValues.password
           );
         });
-        console.log(test2);
-        if (test2.length > 1) {
+
+        if (result.length > 0) {
           alert("login Succesfull");
+          navigate("/");
         } else {
           setError("User Doesn't exist");
           alert("User Doesn't exist");
+          setError("");
         }
       })
       .catch((error) => console.log(error));
   };
 
   return (
-    <div>
+    <div className="form">
+      <h1>Login</h1>
       <form className="auth_form">
         <input
           type="name"
           className="name"
           placeholder="Enter Name"
-          value={initialvalues.name}
+          name="name"
           onChange={handleChange}
+          value={inputValues.name}
         />
         <br />
         <input
           type="password"
           className="password"
           placeholder="Enter password"
-          value={initialvalues.password}
           onChange={handleChange}
+          name="password"
+          value={inputValues.password}
         />
         <br />
-        <button onClick={handleSubmit}>Submit</button>
+        <button id="submit" onClick={handlelogin}>
+          Submit
+        </button>
       </form>
       <h3 className="error-container">{error}</h3>
     </div>
